2a421b0fdc9de25863f43c5ac692675f59bd716f
Ivo van der Wijk <github@in.m3r.nl>
2013-06-25 17:15:00 +0200

    implementation of atom/rss091/rss2 feeds

---
+++ wheelcms_rss/feeds.py
@@ -0,0 +1,56 @@
+from django.contrib.syndication.views import Feed
+from django.utils import feedgenerator
+
+from wheelcms_axle.actions import action_registry
+from wheelcms_axle.content import Content
+
+
+class WheelFeed(Feed):
+
+    def __init__(self, instance):
+        super(WheelFeed, self).__init__()
+        self.instance = instance
+
+    def link(self):
+        return self.instance.node.path + '/+' + self.action
+
+    def title(self):
+        return "Feed for %s" % self.instance.title
+
+    def description(self):
+        return self.instance.description
+
+    def items(self):
+        return Content.objects.filter(node__path__startswith="/")
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.description
+
+    @classmethod
+    def handler(cls, handler, request, action):
+        return cls(handler.instance.content())(request)
+
+class AtomWheelFeed(WheelFeed):
+    action = "atom"
+
+    feed_type = feedgenerator.Atom1Feed
+
+class RSS1WheelFeed(WheelFeed):
+    action = "rss091"
+
+    feed_type = feedgenerator.RssUserland091Feed
+
+class RSS2WheelFeed(WheelFeed):
+    action = "rss2"
+
+    feed_type = feedgenerator.Rss201rev2Feed
+
+
+action_registry.register(RSS1WheelFeed.handler, RSS1WheelFeed.action)
+action_registry.register(RSS2WheelFeed.handler, RSS2WheelFeed.action)
+action_registry.register(RSS2WheelFeed.handler, RSS2WheelFeed.action)
+action_registry.register(AtomWheelFeed.handler, AtomWheelFeed.action)
+
